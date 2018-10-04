6cb48d8bee6b8be9856dd391cd664a8f432afda1
wsammyr <weslleysammyr@gmail.com>
2018-10-04 00:04:00 -0300

    Include validation messages in portuguese for constraints descriptions

--- src/main/java/br/com/spring/rest/docs/config/Application.java
+++ src/main/java/br/com/spring/rest/docs/config/Application.java
@@ -3,11 +3,17 @@ package br.com.spring.rest.docs.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "br.com.spring.rest.docs")
 public class Application extends WebMvcConfigurerAdapter {
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**").addResourceLocations("classpath:/static/docs/");
+    }
 
 }
